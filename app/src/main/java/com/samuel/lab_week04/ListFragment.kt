dd189a760743ce1e7fbb0ec62dba6b73dccd37c4
MBotakk <samuel.itachis4@gmail.com>
2025-09-21 15:29:25 +0700

    Commit No. 2 – Bottom Navigation

---
+++ app/src/main/java/com/samuel/lab_week04/ListFragment.kt
@@ -0,0 +1,44 @@
+package com.samuel.lab_week04
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+
+class ListFragment : Fragment() {
+
+    companion object {
+        const val COFFEE_ID = "COFFEE_ID"
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val coffeeList = listOf<View>(
+            view.findViewById(R.id.affogato),
+            view.findViewById(R.id.americano),
+            view.findViewById(R.id.latte)
+        )
+
+        coffeeList.forEach { coffee ->
+            coffee.setOnClickListener {
+                val fragmentBundle = bundleOf(COFFEE_ID to coffee.id)
+                view.findNavController().navigate(
+                    R.id.action_listFragment_to_detailFragment,
+                    fragmentBundle
+                )
+            }
+        }
+    }
+}
